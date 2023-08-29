a486d74b2493f6bf8ff2ce8667d55ed45bce95d3
gabriel roux <rouxgab81@gmail.com>
2023-08-29 15:20:03 -0400

    remove controller omniauth for devise to routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
     mount Sidekiq::Web => '/sidekiq'
   end
 
-  devise_for :users,
-    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users
+  # ,
+  #   controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   root to: 'pages#home'
 
